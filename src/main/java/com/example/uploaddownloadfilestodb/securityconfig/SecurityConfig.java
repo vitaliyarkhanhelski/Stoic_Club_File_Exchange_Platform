701677b66c26261d31854c82c8ba86454d0dbdaf
Vitaliy Arkhanhelski <vit667@gmail.com>
2020-10-06 12:55:16 +0200

    3

--- src/main/java/com/example/uploaddownloadfilestodb/securityconfig/SecurityConfig.java
+++ src/main/java/com/example/uploaddownloadfilestodb/securityconfig/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/files")
                 .and()
                 .exceptionHandling().accessDeniedPage("/accessDenied");
 
